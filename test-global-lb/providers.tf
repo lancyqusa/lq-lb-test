7f9da97aafde2dcbce91152bcb0887c3e2d6ef81
Lancy Quadros <lancy.quadros@sada.com>
2023-05-18 22:36:55 -0500

    added google-beta

--- test-global-lb/providers.tf
+++ test-global-lb/providers.tf
@@ -4,3 +4,10 @@ provider "google" {
   region      = var.gcp_region
   zone        = var.gcp_zone
 }
+
+provider "google-beta" {
+#  credentials = file(var.gcp_credentials_file)
+  project     = local.gcp_project_id
+  region      = var.gcp_region
+  zone        = var.gcp_zone
+}
