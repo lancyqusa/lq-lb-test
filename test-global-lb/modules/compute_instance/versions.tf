d0cc24602748c4a6535bf5721427dabe2b84d6c4
Lancy Quadros <lancy.quadros@sada.com>
2023-05-20 13:48:38 -0500

    updated google provider

--- test-global-lb/modules/compute_instance/versions.tf
+++ test-global-lb/modules/compute_instance/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">=0.13.0"
   required_providers {
-    google = ">= 3.43, <4.0"
+    google = ">4.0"
   }
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v6.0.0"
